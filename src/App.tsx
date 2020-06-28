a41a7be981b5546e73fcda8c782a2229f41855e0
Yassine-cheffai <yacincheffai@yahoo.com>
2020-06-28 13:41:12 +0200

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button, Col, DatePicker, Input, Row, Select, Switch } from 'antd';
+import { Button, Col, DatePicker, Input, Row, Select, Switch, Typography } from 'antd';
 import React from 'react';
 import './App.css';
 
 
-
+const { Title } = Typography;
 const { Option } = Select;
 const { RangePicker } = DatePicker;
 
@@ -13,9 +13,19 @@ const children = [<Option key={"1"} value="1">1</Option>,
 const App = () => {
 
     return (
-        <Row justify="center">
-            <Col span={24}>
+      <div style={{ marginTop: "100px"}}>
+        <Row>
+            <Col span={8} offset={8}>
+                <Title level={3} style={{ marginTop: "20px", marginBottom: "100px", textAlign: "center"}}>Twitter DataSets Builder</Title>
+            </Col>
+        </Row>
+        <Row>
+            <Col span={8} offset={8} style={{ padding: '8px 0' }}>
                 <Input placeholder="Keywords" />
+            </Col>
+        </Row>
+        <Row>
+            <Col span={8} offset={8} style={{ padding: '8px 0' }}>
                 <Select
                     mode="multiple"
                     style={{ width: '100%' }}
@@ -23,16 +33,36 @@ const App = () => {
                     defaultValue={['a10', 'c12']}>
                     {children}
                 </Select>
-                <RangePicker />
-                <Switch /> Add semantic analyse
-          <Switch /> Retweets
-          <Switch /> Remove urls
-
-            <Button type="primary" htmlType="submit">
-                    Submit
-            </Button>
             </Col>
         </Row>
+        <Row>
+            <Col span={8} offset={8} style={{ padding: '8px 0' }}>
+                <RangePicker />
+            </Col>
+        </Row>
+        <Row>
+            <Col span={8} offset={8} style={{ padding: '8px 0' }}>
+                <Switch /> Add semantic analyse
+            </Col>
+        </Row>
+        <Row>
+            <Col span={8} offset={8} style={{ padding: '8px 0' }}>
+                <Switch /> Retweets
+            </Col>
+        </Row>
+        <Row>
+            <Col span={8} offset={8} style={{ padding: '8px 0' }}>
+                <Switch /> Remove urls
+            </Col>
+        </Row>
+        <Row>
+            <Col span={4} offset={12} style={{ padding: '8px 0' }}>
+              <Button type="primary" htmlType="submit">
+                      Submit
+              </Button>
+            </Col>
+        </Row>
+      </div>
 
     );
 };
