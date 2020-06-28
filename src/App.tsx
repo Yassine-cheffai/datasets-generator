4752b5ac2220734c44c3e196b98ca89591539a7e
Yassine-cheffai <yacincheffai@yahoo.com>
2020-06-28 05:02:20 +0200

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
+import { Button, Col, DatePicker, Input, Row, Select, Switch } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. by yassine cheffai
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+
+const children = [<Option key={"1"} value="1">1</Option>,
+<Option key={"2"} value="2">2</Option>,];
+
+const App = () => {
+
+    return (
+        <Row justify="center">
+            <Col span={24}>
+                <Input placeholder="Keywords" />
+                <Select
+                    mode="multiple"
+                    style={{ width: '100%' }}
+                    placeholder="Please select fields"
+                    defaultValue={['a10', 'c12']}>
+                    {children}
+                </Select>
+                <RangePicker />
+                <Switch /> Add semantic analyse
+          <Switch /> Retweets
+          <Switch /> Remove urls
+
+            <Button type="primary" htmlType="submit">
+                    Submit
+            </Button>
+            </Col>
+        </Row>
+
+    );
+};
 
 export default App;
