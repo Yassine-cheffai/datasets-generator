04f8418a6195bdb48321a5966093d10d3f96df3b
Yassine-cheffai <yacincheffai@yahoo.com>
2020-12-14 14:21:05 +0100

    add fields choices

--- src/reddit.tsx
+++ src/reddit.tsx
@@ -2,32 +2,38 @@ import React, { useState } from 'react';
 import { Row, Col, Select, Input, Button} from 'antd';
 import axios from 'axios'
 import { CSVLink } from "react-csv";
-import { DownloadOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { DownloadOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
+const children = [
+    <Option key={"title"} value="title">Submission Title</Option>,
+    <Option key={"author"} value="author">Author</Option>,
+    <Option key={"created_utc"} value="created_utc">Creation Date (utc)</Option>,
+    <Option key={"num_comments"} value="num_comments">Number of comments</Option>,
+    <Option key={"selftext"} value="selftext">Sumission Content</Option>,
+                  ];
 export const Reddit = () => {
     const [searchType, setsearchType] = useState("specific_subreddit");
     const [keywords, setKeywords] = useState("");
     const [csvData, setcsvData] = useState([]);
     const [isReady, setisReady] = useState(false);
     const [isLoading, setisLoading] = useState(false);
+    const [fields, setFields] = useState(['title', ]);
 
     const submit = () => {
         setisLoading(true);
         axios.post((process.env.REACT_APP_BACKEND ? process.env.REACT_APP_BACKEND : "http://127.0.0.1:8000") + "reddit/", {
             keywords: keywords,
             search_type: searchType,
-            csv_fields: ["title", "author"]
+            csv_fields: fields,
         }
         )
              .then(res => {
-                 console.log(res);
                  setcsvData(res.data.result)
                  setisReady(true);
                  setisLoading(false);
              })
              .catch(error => {
-                 console.log(error);
                  setisLoading(false);
              })
     }
@@ -47,6 +53,18 @@ export const Reddit = () => {
                        onChange={(event) => {setKeywords(event.target.value);}}
                        value={keywords}
                 />
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{ padding: '8px 0', width: "100%" }}>
+                    <Select
+                        mode="multiple"
+                        style={{ width: '100%' }}
+                        placeholder="Please select fields"
+                        defaultValue={['title']}
+                        onChange={event => setFields(event)}>
+                        {children}
+                    </Select>
                 </Col>
             </Row>
             <Row>
